071d5321d6ef1df693d57f485aaedeb3be555cb3
k1hp9052 <k1hp9052@gmail.com>
2017-12-21 15:10:37 +0900

    jsp

---
+++ iot_jsp/src/com/test/iot/service/UserService.java
@@ -0,0 +1,11 @@
+package com.test.iot.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public interface UserService {
+
+	public ArrayList<HashMap<String,Object>> getUserLIst();
+	public HashMap<String,Object>getUser();
+	
+}
---
+++ iot_jsp/src/com/test/iot/service/impl/UserServiceImpl.java
@@ -0,0 +1,25 @@
+package com.test.iot.service.impl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.test.iot.service.UserService;
+
+public class UserServiceImpl implements UserService{
+
+	@Override
+	public ArrayList<HashMap<String, Object>> getUserLIst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HashMap<String, Object> getUser() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+	
+}
